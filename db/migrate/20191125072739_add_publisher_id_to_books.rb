d7282a77ba28f9d14a4cc125cc5f9c5d9df86713
蕭凱文 <zozo2936@gmail.com>
2019-12-11 16:00:04 +0800

    修好了！！！！！！

--- db/migrate/20191125072739_add_publisher_id_to_books.rb
+++ db/migrate/20191125072739_add_publisher_id_to_books.rb
@@ -3,6 +3,6 @@ class AddPublisherIdToBooks < ActiveRecord::Migration[6.0]
     # add_column :books, :publisher_id, :integer
     # add_index :books, :publisher_id
 
-    add_reference :books, :publisher_id
+    add_reference :books, :publisher
   end
 end
